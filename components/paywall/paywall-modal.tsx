40c679a681f80d1b3bcb214e542f839793d6edb7
makaraq <makaraq@users.noreply.github.com>
2026-05-18 17:05:59 -0400

    Update paywall-modal.tsx

--- components/paywall/paywall-modal.tsx
+++ components/paywall/paywall-modal.tsx
@@ -133,7 +133,7 @@ export function PaywallModal({
             {selectedPlan === 'yearly' ? (
               <>Start your 7-day FREE<br />trial to continue.</>
             ) : (
-              <>Get the full Sprind experience</>
+              <>Get the full Sprind experience now</>
             )}
           </h2>
         </div>
